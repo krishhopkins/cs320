6857ffd4aa98a46cb31617e021cff39b2e922c70
Osamu Fujimoto <oaf7862@rit.edu>
2015-05-05 06:07:04 -0400

    initial database schema completed

---
+++ database/migrations/2015_05_01_120000_create_char_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('char', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('owner_id')->unsigned();
+			$table->string('name')->unique();
+			$table->integer('level');
+			$table->integer('gold');
+			$table->integer('experience');
+			$table->enum("class", 
+				array('WARRIOR', 'MAGE', 'HEALER', 'MONK'));
+			$table->integer('current_location')->unsigned();
+			$table->integer('hp');
+			$table->integer('str');
+			$table->integer('dex');
+			$table->integer('int');
+			$table->integer('luk');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('char');
+	}
+
+}
